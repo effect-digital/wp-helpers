666a58853a8eed9b1f3ff2d7eeb93dadf22212e4
Christian Thomas <christian@industrious.agency>
2020-02-19 20:28:39 +0000

    Add noopener to Twitter links

--- src/Fetchers/Other/TweetFetcher.php
+++ src/Fetchers/Other/TweetFetcher.php
@@ -186,10 +186,10 @@ class TweetFetcher extends TransientFetcher
     private function twitterify($content = '')
     {
         $content = preg_replace('/([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})/', '<a href="mailto:$1">$1</a>', $content);
-        $content = preg_replace("#(^|[\n ])([\w]+?://[\w]+[^ \"\n\r\t< ]*)#", "\\1<a href=\"\\2\" target=\"_blank\">\\2</a>", $content);
-        $content = preg_replace("#(^|[\n ])((www|ftp)\.[^ \"\t\n\r< ]*)#", "\\1<a href=\"http://\\2\" target=\"_blank\">\\2</a>", $content);
-        $content = preg_replace("/^@(\w+)/", "<a href=\"http://www.twitter.com/\\1\" target=\"_blank\">@\\1</a>", $content);
-        $content = preg_replace("/#(\w+)/", "<a href=\"http://twitter.com/search?q=\\1\" target=\"_blank\">#\\1</a>", $content);
+        $content = preg_replace("#(^|[\n ])([\w]+?://[\w]+[^ \"\n\r\t< ]*)#", "\\1<a href=\"\\2\" target=\"_blank\" rel=\"noopener\">\\2</a>", $content);
+        $content = preg_replace("#(^|[\n ])((www|ftp)\.[^ \"\t\n\r< ]*)#", "\\1<a href=\"http://\\2\" target=\"_blank\" rel=\"noopener\">\\2</a>", $content);
+        $content = preg_replace("/^@(\w+)/", "<a href=\"http://www.twitter.com/\\1\" target=\"_blank\" rel=\"noopener\">@\\1</a>", $content);
+        $content = preg_replace("/#(\w+)/", "<a href=\"http://twitter.com/search?q=\\1\" target=\"_blank\" rel=\"noopener\">#\\1</a>", $content);
 
         return $content;
     }
